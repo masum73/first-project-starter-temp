9d84c4f3ddf3be9b7a8f105adb74a786435367d9
Masum Rahman <masumrahman73@gmail.com>
2023-12-09 22:46:43 +0600

    course modules added , module 15 full complete

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -71,8 +71,8 @@ const getAllStudents = catchAsync(async (req, res) => {
 });
 
 const getSingleStudent = catchAsync(async (req, res) => {
-  const studentId = req.params.studentId;
-  const result = await StudentServices.getSingleStudentFromDB(studentId);
+  const id = req.params._id;
+  const result = await StudentServices.getSingleStudentFromDB(id);
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
@@ -82,9 +82,9 @@ const getSingleStudent = catchAsync(async (req, res) => {
 });
 
 const updateStudent = catchAsync(async (req, res) => {
-  const { studentId } = req.params;
+  const { id } = req.params;
   const { student } = req.body;
-  const result = await StudentServices.updateStudentIntoDB(studentId, student);
+  const result = await StudentServices.updateStudentIntoDB(id, student);
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
@@ -95,8 +95,8 @@ const updateStudent = catchAsync(async (req, res) => {
 });
 
 const deleteStudent = catchAsync(async (req, res) => {
-  const studentId = req.params.studentId;
-  const result = await StudentServices.deleteStudentFromDB(studentId);
+  const id = req.params._id;
+  const result = await StudentServices.deleteStudentFromDB(id);
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
